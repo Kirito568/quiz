8e83cc348f0d0c14b301aa7249153869b0cf4047
Ryosuke Otsuki <crafters0301@gmail.com>
2019-11-18 15:39:02 +0900

    validation in result.php

--- result.php
+++ result.php
@@ -1,8 +1,13 @@
 <?php
 require_once('common.php');
 
-$qed= $_GET['qed'];
+$qed=isset ($_GET['qed'])?$_GET['qed']:-1;
 $answer= $_GET['answer'];
+//Validation
+if($qed== -1 || !is_numeric($qed)|| !((0<=$qed)&&($qed<count($question))) ){
+    echo 'Error: $qed invalid';
+    exit(1);
+}
 
 if($question[$qed][1]==$answer){
     echo "Correct!";
