b7093cf6f08c9b13306dbe61fd729c6b67705369
Ryosuke Otsuki <crafters0301@gmail.com>
2019-11-25 16:21:20 +0900

    191125 modify

--- question.php
+++ question.php
@@ -15,10 +15,25 @@ $i = rand(0,count($question)-1);
 
     <form action="result.php">
         <input type="hidden" name="qed" value="<?= $i ?>">
-        <input type="text" name="answer">
-        <button>answer</button>
+        <input type="text" id="text-answer" name="answer">
+        <button id="btn-answer">answer</button>
     </form>
+
+<script>
+document.querySelector("#btn-answer").addEventListener("click",(e)=>{
+    /*alert("Button has pushed!");*/   
+    let answer = document.querySelector("#text-answer");
     
+    if(answer.value == ""){
+        alert("please input here.");
+        answer.focus();
+        answer.style.backgroundColor="Pink";
+        e.preventDefault(); 
+    }else{
+    document.querySelector("#btn-answer").innerHTML="sending...";
+    }
+});
+</script>
 </body>
 
 
--- result.php
+++ result.php
@@ -1,16 +1,41 @@
 <?php
+
 require_once('common.php');
 
-$qed=isset ($_GET['qed'])?$_GET['qed']:-1;
-$answer= $_GET['answer'];
-//Validation
-if($qed== -1 || !is_numeric($qed)|| !((0<=$qed)&&($qed<count($question))) ){
-    echo 'Error: $qed invalid';
-    exit(1);
+
+
+// 引数（クエリー）を受け取る
+
+$qed    = isset($_GET['qed'])? $_GET['qed']:-1; 
+
+$answer = $_GET['answer'];
+
+
+
+// Validation
+
+if($qed == -1 || !is_numeric($qed) || !((0<=$qed) && ($qed<count($question))) ){
+
+	echo 'error: $qed invalid';
+
+	exit(1);
+
 }
 
-if($question[$qed][1]==$answer){
-    echo "Correct!";
-}else{
-    echo "Incorrect...";
+
+
+
+
+// 正解か判定
+
+if( $question[$qed][1] == $answer ){
+
+	echo "正解！";
+
+}
+
+else{
+
+	echo "残念！";
+
 }
